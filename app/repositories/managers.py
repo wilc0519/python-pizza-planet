2792b21fb0b6102c307a75a27bbd5de0dbd98902
wilc0519 <wilc0519@gmail.com>
2022-08-17 12:12:03 -0500

    refactor delete comments

--- app/repositories/managers.py
+++ app/repositories/managers.py
@@ -73,9 +73,6 @@ class OrderManager(BaseManager):
         cls.session.add(new_order)
         cls.session.flush()
         cls.session.refresh(new_order)
-        #cls.session.add_all((OrderDetail(order_id=new_order._id, ingredient_id=ingredient._id, ingredient_price=ingredient.price, beverage_id=beverage._id, beverage_price=beverage.price)
-        #                     for ingredient in ingredients
-        #                     for beverage in beverages))
         cls.session.add_all((OrderDetail(order_id=new_order._id, ingredient_id=ingredient._id, ingredient_price=ingredient.price)
                              for ingredient in ingredients))
         cls.session.add_all((OrderDetail(order_id=new_order._id, beverage_id=beverage._id, beverage_price=beverage.price)
